86ab7805409131694931193ac67e65d058603a5f
Lewis Clark <me@lewez.dev>
2019-07-31 14:27:52 +0100

    Reference System.Net.Http

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@ filenames=$(find . -name "*.cs" | tr '\n' ' ')
 
 echo $filenames
 
-mcs -out:application.exe -r:System.Windows.Forms,System.Drawing $filenames
+mcs -out:application.exe -r:System.Windows.Forms,System.Drawing,System.Net.Http $filenames
